cd36a838cdc3401bf2728877b4bda1e847e47f7f
msavelie <msavelie@student.hive.fi>
2024-05-17 12:12:35 +0300

    Fix header

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: msavelie <msavelie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 15:48:47 by msavelie          #+#    #+#             */
-/*   Updated: 2024/05/17 12:02:49 by msavelie         ###   ########.fr       */
+/*   Updated: 2024/05/17 12:11:35 by msavelie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,14 @@
 # define FT_PRINTF_H
 
 # include <stdarg.h>
+# include <unistd.h>
 
 # define UP_HEX_BASE "0123456789ABCDEF"
 # define LOW_HEX_BASE "0123456789abcdef"
 
 int		ft_printf(const char *data, ...);
 int		ft_putchar(char c);
-int		ft_putnbr(long num, int fd);
+int		ft_putstr(const char *str);
 int		ft_print_hex(long nbr, int base, char c);
 int		print_p(unsigned long num);
 
