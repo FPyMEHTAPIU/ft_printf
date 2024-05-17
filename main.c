11122019423cea517bff01782de131f3c7755c85
msavelie <msavelie@student.hive.fi>
2024-05-17 15:58:25 +0300

    Update main

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "ft_printf.h"
 #include <stdio.h>
+#include <limits.h>
 
 int main(void)
 {
@@ -30,19 +31,35 @@ int main(void)
 	ft_printf("\nMy Void * argument: %p\n", str);
 	int s = 1100000000, w = 1100000000;
 	int *p = malloc(sizeof(int) * 30);
-	printf("Another one: %p\n", 1100000000);
-	printf("Another one: %p\n", &s);
-	printf("Another one: %p\n", &w);
-	printf("Another one: %p\n", p);
+	printf("Another one %%p: %p\n", 1100000000);
+	printf("Another one %%p: %p\n", &s);
+	printf("Another one %%p: %p\n", &w);
+	printf("Another one %%p: %p\n", p);
 
-	ft_printf("My Another one: %p\n", 1100000000);
-	ft_printf("My Another one: %p\n", &s);
-	ft_printf("My Another one: %p\n", &w);
-	ft_printf("My Another one: %p\n", p);
+	ft_printf("My Another one %%p: %p\n", 1100000000);
+	ft_printf("My Another one %%p: %p\n", &s);
+	ft_printf("My Another one %%p: %p\n", &w);
+	ft_printf("My Another one %%p: %p\n", p);
 
 	free(p);
 
-	ft_printf("My: %p %p \n", 0, 0);
-	printf("Original: %p %p \n", 0, 0);
+	ft_printf("My %%p %%p 0 and 0: %p %p \n", 0, 0);
+	printf("Original %%p %%p 0 and 0: %p %p \n\n", 0, 0);
 
+	ft_printf("My %%p %%p -69375981295 and -69375981295: %p %p \n", -69375981295, -69375981295);
+	printf("Original %%p %%p -69375981295 and -69375981295: %p %p \n\n", -69375981295, -69375981295);
+	ft_printf("My %%p %%p -255 and 879544351315: %p %p \n", -255, 879544351315);
+	printf("Original %%p %%p -255 and 879544351315: %p %p \n\n", -255, 879544351315);
+	ft_printf("My %%p %%p INT_MAX and INT_MIN: %p %p \n", INT_MAX, INT_MIN);
+	printf("Original %%p %%p INT_MAX and INT_MIN: %p %p \n\n", INT_MAX, INT_MIN);
+	ft_printf("My %%p %%p LONG_MAX and LONG_MIN: %p %p \n", LONG_MAX, LONG_MIN);
+	printf("Original %%p %%p LONG_MAX and LONG_MIN: %p %p \n\n", LONG_MAX, LONG_MIN);
+	ft_printf("My %%p ULONG_MAX: %p \n", ULONG_MAX);
+	printf("Original %%p ULONG_MAX: %p \n\n", ULONG_MAX);
+	ft_printf("My 7 %% marks: %%%%%%% \n");
+	printf("Original 7 %% marks: %%%%%%% \n\n");
+	ft_printf("My %%u %%u LONG_MAX and LONG_MIN: %u %u \n", LONG_MAX, LONG_MIN);
+	printf("Original %%u %%u LONG_MAX and LONG_MIN: %u %u \n\n", LONG_MAX, LONG_MIN);
+	ft_printf("My %%u ULONG_MAX: %u \n", ULONG_MAX);
+	printf("Original %%u ULONG_MAX: %u \n\n", ULONG_MAX);
 }
